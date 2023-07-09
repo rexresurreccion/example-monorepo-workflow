116b538cf359d30205fd31f51067e7e073ce48d9
rexresurreccion <rexjustin_02@yahoo.com>
2023-07-09 16:53:50 -0500

    dispatch workflows

--- build.py
+++ build.py
@@ -3,12 +3,13 @@ import os
 
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
+GITHUB_REF_NAME = os.environ["GITHUB_REF_NAME"]
 headers = {
   "Accept": "application/vnd.github+json",
   "Authorization": f"Bearer {GITHUB_TOKEN}",
   "X-GitHub-Api-Version": "2022-11-28",
 }
-
+print(f"GITHUB_REF_NAME: {GITHUB_REF_NAME}")
 #  https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#list-repository-workflows
 response = requests.get(
     headers=headers,
@@ -23,7 +24,10 @@ for workflow in response.json()["workflows"]:
         headers=headers,
         url=f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/workflows/{workflow['id']}/dispatches",
         json={
-          "data1": "Hello World",  #  This can be an output from a previous task,
+          "ref": GITHUB_REF_NAME,
+          "inputs": {
+            "data1": "Hello world",  # This can be an output from previous task
+          }.
         },
       )
       response.raise_for_status()
